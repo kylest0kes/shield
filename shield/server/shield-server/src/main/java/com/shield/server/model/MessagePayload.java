c247f28f306aa3d5c4e551f16a24562f47f1e847
kylest0kes <kylestokes1551@gmail.com>
2025-10-12 18:12:23 -0400

    made a messagepayload model

---
+++ shield/server/shield-server/src/main/java/com/shield/server/model/MessagePayload.java
@@ -0,0 +1,28 @@
+package com.shield.server.model;
+
+import java.time.Instant;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class MessagePayload {
+
+    @NotBlank(message = "Message content cannot be blank")
+    @Size(max = 2000, message = "Message content too long")
+    private String content;
+
+    @NotNull(message = "Timestamp is required")
+    private Instant timestamp;
+
+    @NotBlank(message = "Sender ID is required")
+    private String senderId;
+
+    private String messageType;
+}
--- shield/server/shield-server/src/main/java/com/shield/server/model/MessagePayloadModel.java
+++
@@ -1,5 +0,0 @@
-package com.shield.server.model;
-
-public class MessagePayloadModel {
-    
-}
